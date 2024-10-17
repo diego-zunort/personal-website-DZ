7701c89bd754d1fb09d1665b991fb928fd7e7cfb
diego-zunort <diegozunort@gmail.com>
2024-10-17 12:05:57 -0500

    changes oct 17

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -20,13 +20,13 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
                 sequence={[
-                    "Ramiro",
+                    "Diego",
                     1000,
-                    "Game Developer",
+                    "Software engineer",
                     1000,
-                    "Researcher",
+                    "Book reader",
                     1000,
-                    "Teacher",
+                    "Game enjoyer",
                     1000,
                 ]}
                 wrapper="span"
@@ -35,23 +35,15 @@ const HeroSection = () => {
                 />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-          &ldquo;The best way to predict the future is to invent it.&ldquo;
-            — Alan Kay
+          &ldquo; I do things... great things".
           </p>
           <div>
           <Link
-              href="/resumes/Game_Dev_Resume.pdf"
+              href="/resumes/DiegoResume.pdf"
               download="Game_Dev_Resume.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 boder border-white mt-3 text-white"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Software Dev</span>
-            </Link>
-            <Link
-              href="/resumes/Game_Dev_Resume.pdf"
-              download="Game_Dev_Resume.pdf"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 boder border-white mt-3 translate-x-[10px] text-white"
-            >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Game Dev</span>
             </Link>
           </div>
         </motion.div>
@@ -63,7 +55,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/cat.jpeg"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={350}
